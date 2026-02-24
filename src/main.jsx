3222764966033fc92af0a36506288de635d04b56
mohammademon393 <mdemon357ep@gmail.com>
2026-02-24 11:29:32 +0600

    Dessign a responsive navbar and create a jsx component

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Root from './layout/Root';
 import Home from './pages/home/Home';
-
+import Services from './pages/services/Services';
 
 // Create a router instance
 const router = createBrowserRouter([
@@ -16,10 +16,21 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "/services",
+        element: <Services />,
       }
-    ]
-  }
+    ],
+  },
 ]);
+
+// Render the application
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
 
 
 // Render the application
